63a6ccdbd76abad4e8944e5cf5cffafc9b472734
sari-rahul <>
2023-12-14 08:59:19 +0000

    Edit Procfile

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY ='django-insecure-ztfto2hj(fdc_h9^9d@1nr1fk43+$^j0p1qwxdl2#7*_x01sz@'
+SECRET_KEY = 'django-insecure-ztfto2hj(fdc_h9^9d@1nr1fk43+$^j0p1qwxdl2#7*_x01sz@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
