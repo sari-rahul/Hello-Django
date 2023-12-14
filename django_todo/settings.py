a501393337dce8971fdcb8b2e27ccb5ef7a0694f
sari-rahul <>
2023-12-14 09:12:05 +0000

    Add heroku as allowed user in settings.py

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = '2erm2g25fi&()6+d)rp0(1gw!vp^v2aoxpmfebc^1_2gbjc-@#'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['my-app-in-django-c70d500a8e0b.herokuapp.com']
 
 
 # Application definition
